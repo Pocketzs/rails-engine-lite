64799a2c9b8817bfc9f50099420b428e2c51b859
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-07 20:12:12 -0600

    add item request spec for fetching single item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -80,4 +80,50 @@ RSpec.describe "Items API" do
       expect(items_data.keys).to_not include(:relationships)
     end
   end
+
+  describe 'fetch a single item' do
+    it 'returns a single item given an items id' do
+      merchant1 = create(:merchant)
+      item1 = create(:item, merchant: merchant1)
+
+      get "/api/v1/items/#{item1.id}"
+
+      expect(response).to have_http_status(:ok)
+
+      item_json = JSON.parse(response.body, symbolize_names:true)
+      item_data = item_json[:data]
+
+      expect(item_data).to be_a(Hash)
+
+      expect(item_data).to have_key(:id)
+      expect(item_data[:id]).to be_a(String)
+      
+      expect(item_data).to have_key(:type)
+      expect(item_data[:type]).to eq("item")
+
+      expect(item_data).to have_key(:attributes)
+      expect(item_data[:attributes]).to be_a(Hash)
+
+      attributes = item_data[:attributes]
+
+      expect(attributes).to have_key(:name)
+      expect(attributes[:name]).to be_a(String)
+      
+      expect(attributes).to have_key(:description)
+      expect(attributes[:description]).to be_a(String)
+     
+      expect(attributes).to have_key(:unit_price)
+      expect(attributes[:unit_price]).to be_a(Float)
+    end
+
+    it 'returns error if item id doesnt exist' do
+      get "/api/v1/items/1"
+
+      error_json = JSON.parse(response.body, symbolize_names:true)
+      # require 'pry'; binding.pry
+
+      expect(response).to have_http_status(:not_found)
+      expect(response.status).to eq(404)
+    end
+  end
 end
