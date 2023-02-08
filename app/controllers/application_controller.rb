e30668985d402779f886201e682303d5c0742a97
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-07 22:47:23 -0600

    add item creation sad path validation functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::API
-  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  # def render_unprocessable_entity_response(exception)
-  #   render json: exception.record.errors, status: :unprocessable_entity
-  # end
+  def render_unprocessable_entity_response(exception)
+    # require 'pry'; binding.pry
+    render json: { message: "Record Invalid", errors: [detail: exception.message, status: "422"] }, status: :unprocessable_entity
+  end
 
   def render_not_found_response(exception)
     # require 'pry'; binding.pry
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  validates_presence_of :name, :description, :unit_price
+  validates_numericality_of :unit_price, greater_than_or_equal_to: 0
 end
