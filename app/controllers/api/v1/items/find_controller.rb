05f10807b3dcd6ed91a2c3b86380002e519c7fe5
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-10 01:56:07 -0600

    create conditional paths for item find controller

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::Items::FindController < ApplicationController
   def index
-    # require 'pry'; binding.pry
-    item = Item.find_item_by_name(params[:name])
-    render json: ItemSerializer.new(item)
+    if params[:name] && (params[:min_price].nil? && params[:max_price].nil?)
+      item = Item.find_item_by_name(params[:name])
+      render json: ItemSerializer.new(item)
+    elsif (params[:min_price] || params[:max_price]) && params[:name].nil?
+      item = Item.find_item_by_unit_price(params[:min_price], params[:max_price])
+      render json: ItemSerializer.new(item)
+    else
+      raise ActiveRecord::RecordInvalid
+    end
   end
 end
