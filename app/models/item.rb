aa7ae9353a6dbd455ec4d3f66e4974b72dc50c8a
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-09 20:24:20 -0600

    add item class method find_item_by_name with error raise

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,22 @@ class Item < ApplicationRecord
   validates_numericality_of :unit_price, greater_than_or_equal_to: 0
   after_destroy :destroy_empty_invoices
 
+  def self.find_item_by_name(query)
+    # where('lower(name) like ?', "%#{query.downcase}%").order(:name).limit(1).first
+    raise ActiveRecord::RecordInvalid unless is_valid_string?(query)
+    # Item.order(:name).find_by!('lower(name) like ?', "%#{query.downcase}%")
+    Item.order(:name).find_by!('name ilike ?', "%#{query}%")
+  end
+
   private
 
   def destroy_empty_invoices
     Invoice.destroy_all
+  end
+
+  def self.is_valid_string?(query)
+    return false if query.nil?
+    true unless query.empty? || query.match?(/[^a-zA-Z]+/)
+    # require 'pry'; binding.pry
   end
 end
