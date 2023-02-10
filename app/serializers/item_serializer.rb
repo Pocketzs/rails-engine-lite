602e9a89130f7ac1c0ae8bd0e8d73ab308082c9f
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-10 01:56:28 -0600

    convert item serializer to handrolled

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,46 @@
 class ItemSerializer
-  include JSONAPI::Serializer
-  attributes :name, :description, :unit_price, :merchant_id
+  def self.new(object)
+    if object.nil?
+      { data: {
+        id: nil,
+        type: nil,
+        attributes: {
+          name: nil,
+          description: nil,
+          unit_price: nil,
+          merchant_id: nil
+        }
+      } }
+    elsif object.class == Item
+      { 
+        data: 
+          {
+            id: object.id.to_s,
+            type: 'item',
+            attributes: 
+              {
+                name: object.name,
+                description: object.description,
+                unit_price: object.unit_price,
+                merchant_id: object.merchant.id
+              }
+          } 
+      }
+    else
+      { 
+        data: object.map do |item|
+          {
+            id: item.id.to_s,
+            type: 'item',
+            attributes: {
+              name: item.name,
+              description: item.description,
+              unit_price: item.unit_price,
+              merchant_id: item.merchant.id
+              }
+          } 
+        end
+      }
+    end
+  end
 end
