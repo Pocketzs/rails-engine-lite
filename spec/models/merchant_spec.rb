3f17d83a00a0f1cd1478896550aa9fc501b66aab
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-10 02:24:05 -0600

    add class method spec for merchant find merchants by name

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Merchant, type: :model do
     it { should have_many :items }
     it { should have_many :invoices }
   end
+
+  describe 'class methods' do
+    describe '.find_merchants_by_name' do
+      it 'returns merchant matched with name' do
+        merchant1 = create(:merchant, name: 'Walmart')
+        merchant2 = create(:merchant, name: 'Allmart')
+
+        expect(Merchant.find_merchants_by_name('MaRt')).to eq([merchant2, merchant1])
+      end
+    end
+  end
 end
