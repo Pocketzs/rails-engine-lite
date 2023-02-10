7c5f45bb8e560eb23d1580846ac89ff9a5f270f5
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2023-02-10 02:17:55 -0600

    add merchant model class methods for find merchants by name

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,16 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+
+  def self.find_merchants_by_name(query)
+    raise ActiveRecord::RecordInvalid unless valid_name?(query)
+    order(:name).where('name ilike ?', "%#{query}%")
+  end
+
+  private
+
+  def self.valid_name?(query)
+    return false if query.nil?
+    true unless query.empty? || query.match?(/[^a-zA-Z]+/)
+  end
 end
